1e2bf9a52f4b724de9baebc0abaad59b8d11adf8
Max Jonas Werner <mail@makk.es>
2010-09-27 18:26:37 +0200

    Added more methods to analyze an FB feed

--- src/de/maxwerner/ffa/FacebookFeedAnalyzer.java
+++ src/de/maxwerner/ffa/FacebookFeedAnalyzer.java
@@ -14,14 +14,18 @@ package de.maxwerner.ffa;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Map.Entry;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,51 +37,78 @@ import org.json.JSONObject;
  * @author Max Jonas Werner <max.jonas.werner@justsoftwareag.com>
  */
 public class FacebookFeedAnalyzer {
-
-    private final static DateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-    private final static DateFormat outputDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+    
+    private final static DateFormat INPUT_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+    private final static DateFormat OUTPUT_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
     public static void main(String[] args) throws IOException, JSONException, ParseException {
         final FacebookFeedAnalyzer analyzer = new FacebookFeedAnalyzer();
         final URL baseUrl = new URL("https://graph.facebook.com/weltkompakt/feed");
         final JSONObject feed = analyzer.getFeed(baseUrl);
-        final JSONArray feedData = feed.getJSONArray("data");
 
-        final Map<String, Integer> commentsPerType = new HashMap<String, Integer>();
-        final Map<String, Integer> postsPerType = new HashMap<String, Integer>();
+        final Calendar from = Calendar.getInstance();
+        final Calendar to = Calendar.getInstance();
+        from.set(2010, Calendar.AUGUST, 27, 23, 59, 59);
+        to.set(2010, Calendar.SEPTEMBER, 26, 0, 0, 0);
+        final List<JSONObject> messages = analyzer.getMessagesBetween(feed, from.getTime(), to.getTime());
+        System.out.println("Total messages: " + messages.size());
+        
+        final Map<String, Integer> messageCountByType = new HashMap<String, Integer>(messages.size());
+        for(JSONObject message : messages) {
+            final String type = message.getString("type");
+            Integer count = messageCountByType.get(type);
+            if(count == null) {
+                messageCountByType.put(type, 1);
+            } else {
+                messageCountByType.put(type, count + 1);
+            }
+        }
+        for(Entry<String, Integer> entry : messageCountByType.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+    
+    /**
+     * Extracts a set of messages from a feed which have been created
+     * after 'from' and before 'to'. Important: the parameters 'from' and
+     * 'to' are exclusive.
+     * 
+     * @param feed the feed to extract messages from
+     * @param from message must have been created after this date
+     * @param to message must have been created before this date
+     * @return the set of messages
+     * @throws ParseException
+     * @throws JSONException
+     * @throws IOException 
+     * @throws MalformedURLException 
+     */
+    public List<JSONObject> getMessagesBetween(final JSONObject feed, final Date from, final Date to) throws ParseException,
+            JSONException, MalformedURLException, IOException {
 
+      final List<JSONObject> result = new ArrayList<JSONObject>();
+      final JSONArray feedData = feed.getJSONArray("data");
+      if(feedData.length() == 0) {
+          return result;
+      }
+        
         for (int i = 0; i < feedData.length(); i++) {
-            final JSONObject entry = feedData.getJSONObject(i);
-            final String type = entry.getString("type");
-            
-            final JSONObject comments = entry.optJSONObject("comments");
-            final Integer commentCount = comments == null ? 0 : new Integer(comments.getInt("count"));
-            if(entry.getJSONObject("from").getString("id").equals("49522294277")) {
-                postsPerType.put(type, postsPerType.get(type) == null ? 1 : postsPerType.get(type) + 1);
-            }
-
-            commentsPerType.put(type, commentsPerType.get(type) != null ? 
-                    commentsPerType.get(type) + commentCount
-                    : commentCount);
-        }
-
-        for(String type : postsPerType.keySet()) {
-            System.out.println(type + ": " + postsPerType.get(type) + ", " + commentsPerType.get(type));
-        }
-
-    }
-
-    public Set<String> getTypesInFeed(final JSONArray feed) throws JSONException {
-        final Set<String> types = new HashSet<String>();
-        for(int i = 0; i < feed.length(); i++) {
-            final JSONObject entry = feed.getJSONObject(i);
-            if(!types.contains(entry.getString("type"))) {
-                types.add(entry.getString("type"));
+            final Date createdTime = INPUT_DATE_FORMAT.parse(feedData.getJSONObject(i).getString("created_time"));
+            if(createdTime.before(to) && createdTime.after(from)) {
+                result.add(feedData.getJSONObject(i));
             }
         }
-        return types;
+        if(from.before(INPUT_DATE_FORMAT.parse(feedData.getJSONObject(feedData.length()-1).getString("created_time")))) {
+            final List<JSONObject> nextResult = getMessagesBetween(getNextPage(feed), from, to);
+            result.addAll(nextResult);
+        }
+        return result;
     }
-
+    
+    public JSONObject getNextPage(final JSONObject feed) throws JSONException, MalformedURLException, IOException {
+        final String nextUrl = feed.getJSONObject("paging").getString("next");
+        return getFeed(new URL(nextUrl));
+    }
+    
     public JSONObject getFeed(final URL baseUrl) throws IOException, JSONException {
         final InputStream is = baseUrl.openStream();
         final int blockSize = 8192;
